84537ff98e726baa9bf409cfe32b133a0df0b93e
arulganz <syahrulrahmadan819@gmail.com>
2021-10-19 06:32:25 +0700

    Self

---
+++ message/donasi.js
@@ -0,0 +1,14 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu owner kembangkan bot ini
+│  dengan donasi seiklasnya 🙂
+│
+│  Dana : 0858-6913-4434 
+│ 
+│ Makasih yang udah donasi
+│ dan bantu ngembangin bot ini
+│
+╰────────⏣`
+}
